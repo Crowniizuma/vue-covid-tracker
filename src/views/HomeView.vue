6e2cdf48fe5bd7245d034ba87588b474dd3c673a
nguyen_anh <nguyen_anh@5star.co.jp>
2022-06-06 12:08:07 +0900

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import DataTitle from '@/components/DataTitle'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    DataTitle
+  },
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '2022-06-06T00:52:54.553Z',
+      status: {},
+      countries: []
+    }
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch('https://api.covid19api.com/summary');
+      const data = await res.json();
+      return data;
+    }
+  },
+  created() {
+    const data = this.fetchCovidData();
+    console.log(data);
+    this.dataDate = data.Date;
+    console.log(data.Date);
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
   }
 }
 </script>
